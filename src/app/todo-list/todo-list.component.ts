35f93c64d66154dd96a75a44301f659c4b6ff017
Piotr Konczyk <konczykp@zsl230.local>
2024-10-29 13:00:46 +0100

    chore: added removing completed todos

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Todo } from '../../types/todo';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgForOf, NgIf, NgStyle} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    NgStyle,
+    FormsModule
   ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
@@ -21,15 +24,15 @@ export class TodoListComponent {
 
   todos: Todo[] = []
 
-  addTodo(title: string, description: string) {
-    if(!title.length || !description.length) {
+  addTodo() {
+    if(!this.title.length || !this.description.length) {
       this.errorMessage = "Please fill in all fields"
       return
     }
 
     this.todos.push({
-      title: title,
-      description: description,
+      title: this.title,
+      description: this.description,
       isCompleted: false
     })
 
@@ -40,6 +43,14 @@ export class TodoListComponent {
 
   completeTodo(todo: Todo) {
     todo.isCompleted = !todo.isCompleted;
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter(t => t !== todo)
+  }
+
+  deleteCompletedTodos() {
+    this.todos = this.todos.filter(t => !t.isCompleted)
   }
 
   closeAlert() {
