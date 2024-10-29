709c7ed95f2646ed9550b149bdef8e3d6af15b0e
Piotr Konczyk <konczykp@zsl230.local>
2024-10-29 12:13:15 +0100

    chore: added error message

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,27 +1,48 @@
 import { Component } from '@angular/core';
 import { Todo } from '../../types/todo';
-import {of} from 'rxjs';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
+
 export class TodoListComponent {
   title = ""
   description = ""
 
+  errorMessage = ""
+
   todos: Todo[] = []
 
   addTodo(title: string, description: string) {
+    if(!title.length || !description.length) {
+      this.errorMessage = "Please fill in all fields"
+      return
+    }
+
     this.todos.push({
       title: title,
       description: description,
       isCompleted: false
     })
+
+    this.title = ""
+    this.description = ""
+    this.errorMessage = ""
   }
 
-  protected readonly of = of;
+  completeTodo(todo: Todo) {
+    todo.isCompleted = !todo.isCompleted;
+  }
+
+  closeAlert() {
+    this.errorMessage = ""
+  }
 }
